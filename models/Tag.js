cc6f8929981931b1725d52a91553af89fb5e28f7
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-05-31 12:57:15 +1000

    Define Tag sequelize model

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    tag_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,

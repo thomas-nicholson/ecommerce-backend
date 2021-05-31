5a827c52a85eeccf5935b4ae6ceb3157dc3a8fdf
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-05-31 18:43:57 +1000

    Added category update route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,11 +42,22 @@ router.post('/', async (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        category_id  : req.params.id,
+      },
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 

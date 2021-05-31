9807f26e4b4a3778c2c30b5c3883093dd9a7490a
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-05-31 19:45:52 +1000

    Added delete category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,6 +59,15 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {category_id: req.params.id},
+    });
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
